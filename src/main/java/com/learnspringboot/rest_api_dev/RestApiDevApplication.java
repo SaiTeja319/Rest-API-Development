aed135c759824fcabaef858ee921089d48200049
lagga_s <saiteja.laggani@gmail.com>
2025-05-21 16:07:08 +0530

    Initial commit

---
+++ src/main/java/com/learnspringboot/rest_api_dev/RestApiDevApplication.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.rest_api_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnspringboot/rest_api_dev/RestApiDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.rest_api_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
